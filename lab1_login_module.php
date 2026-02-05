83e7633c8b03cd90fac31a3b0967f3a4ab8bc91e
bhargavi-220226 <n220226@rguktn.ac.in>
2026-02-05 00:36:48 +0000

    modified login module php file

--- lab1_login_module.php
+++ lab1_login_module.php
@@ -2,7 +2,7 @@
 $email = $_POST['email'];
 $password =$_POST['password'];
 $correct_mail ="bhargavi.allaka369@gmail.com";
-$correct_password="teja@2007";
+$correct_password="bhargavi369";
 if ($email === $correct_mail && $password === $correct_password) {
     echo "Login Successful<br>";
     print "Welcome ";
